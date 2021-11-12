5547a08a2350b49cf7983b4c9d8d7529f2ef49a6
Burkhant Suerfu <suerfu@berkeley.edu>
2021-11-12 10:38:36 -0800

    UI directly loads macro from macro directory.

--- DetectorTemplate.cc
+++ DetectorTemplate.cc
@@ -139,9 +139,9 @@ int main( int argc, char** argv ){
         if( visManager!=0 ){
             visManager->Initialize();
         }
-        UImanager->ApplyCommand("/control/execute init_vis.mac");
+        UImanager->ApplyCommand("/control/execute macros/init_vis.mac");
         if (ui->IsGUI()) {
-            UImanager->ApplyCommand("/control/execute gui.mac");
+            UImanager->ApplyCommand("/control/execute macros/gui.mac");
         }
         ui->SessionStart();
     }
