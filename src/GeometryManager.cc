40ff4d09eaf2e9a618d627b4486d1e5b73970d56
Suerfu Burkhant <suerfu64@gmail.com>
2023-05-21 19:13:05 +0900

    Added Kamioka rock composition.

--- src/GeometryManager.cc
+++ src/GeometryManager.cc
@@ -86,8 +86,6 @@ void  GeometryManager::DefineMaterials( ){
     G4Material* LHe = new G4Material( "LHe", z=2., a= 4.00*g/mole, density= 0.141*g/cm3 );
 	materialColor["LHe"] = G4Color( 1, 0.1, 1 );
 
-
-
     // ***********************************
 	// Sodium Iodide
     // ***********************************
@@ -141,7 +139,8 @@ void  GeometryManager::DefineMaterials( ){
 
 
     // ***********************************
-	// Rock at Homestake
+	// Rock at Kamioka
+	// taken from https://journals.aps.org/prd/pdf/10.1103/PhysRevD.74.053007
     // ***********************************
     //
     G4double fracM;
@@ -149,11 +148,7 @@ void  GeometryManager::DefineMaterials( ){
     
     G4int nComponents;
 
-    G4Material* rock = new G4Material("Rock", density= 3.26*g/cm3, nComponents = 12);
-        // The density for Homestake formation is taken from:
-        //      https://sdaos.org/wp-content/uploads/pdfs/2014/roggenthen%2033-41.pdf
-        // The chemical composition is taken from:
-        //      https://arxiv.org/pdf/0912.0211.pdf
+    G4Material* rock = new G4Material("Rock", density= 3.26*g/cm3, nComponents = 14);
 
     G4Element* elSi = new G4Element( "Silicon", symbol = "Si" , z = 14., a = 28.086*g/mole);
     G4Element* elTi = new G4Element( "Titanium", symbol = "Ti" , z = 22., a = 47.867*g/mole);
@@ -168,6 +163,7 @@ void  GeometryManager::DefineMaterials( ){
     G4Element* elP  = new G4Element( "Phosphorus", symbol = "P", z = 15., a = 30.974*g/mole);
     G4Element* elH  = new G4Element( "Hydrogen", symbol = "H", z = 1., a = 1.008*g/mole);
     G4Element* elO  = new G4Element( "Oxygen", symbol = "O", z = 8., a = 15.999*g/mole);
+	G4Element* elS  = new G4Element( "Sulfur", symbol = "S", z = 16., a = 32.065*g/mole);
 
     G4double fracM_O = 0;
         // used to accumulate fractional mass of oxygen in the rock
@@ -175,74 +171,92 @@ void  GeometryManager::DefineMaterials( ){
         // fraction of weight of the composite in the rock.
 
     // ============ SiO2 =============
-    fraction = 0.437;
-    fracM = 28.086/(28.086+2*15.999);
+    fraction = 0.607;
+    fracM = 28.086 / ( 28.086 + 2*15.999 );
     fracM_O += (1-fracM)*fraction;
     rock->AddElement( elSi, fracM * fraction );
 
     // ============ TiO2 =============
-    fraction = 0.0122;
-    fracM = 47.867/(47.867+2*15.999);
+    fraction = 0.0031;
+    fracM = 47.867 / ( 47.867 + 2*15.999 );
     fracM_O += (1-fracM)*fraction;
     rock->AddElement( elTi, fracM * fraction );
 
     // ============ Al2O3 =============
-    fraction = 0.136;
-    fracM = 2*26.981/(2*26.981+3*15.999);
+    fraction = 0.1739;
+    fracM = 2*26.981 / ( 2*26.981 + 3*15.999 );
     fracM_O += (1-fracM)*fraction;
     rock->AddElement( elAl, fracM * fraction );
 
-    // ============ FeO =============
-    fraction = 0.127;
-    fracM = 55.845/(55.845+15.999);
+    // ============ FeO and Fe2O3 =============
+    fraction = 0.0122;
+    fracM = 55.845 / ( 55.845 + 15.999 );
     fracM_O += (1-fracM)*fraction;
-    rock->AddElement( elFe, fracM * fraction );
+	G4double frac1 = fracM * fraction;
+
+	fraction = 0.011;
+    fracM = 2*55.845 / ( 2*55.845 + 3*15.999 );
+    fracM_O += (1-fracM)*fraction;
+	G4double frac2 = fracM * fraction;
+
+    rock->AddElement( elFe, frac1+frac2 );
 
     // ============ MnO =============
-    fraction = 0.0013;
-    fracM = 54.938/(54.938+15.999);
+    fraction = 0.0015;
+    fracM = 54.938 / ( 54.938 + 15.999 );
     fracM_O += (1-fracM)*fraction;
     rock->AddElement( elMn, fracM * fraction );
 
     // ============ MgO =============
-    fraction = 0.07;
-    fracM = 24.405/(24.405+15.999);
+    fraction = 0.0093;
+    fracM = 24.405 / ( 24.405 + 15.999 );
     fracM_O += (1-fracM)*fraction;
     rock->AddElement( elMg, fracM * fraction );
 
     // ============ CaO =============
-    fraction = 0.079;
+    fraction = 0.06;
     fracM = 40.078 / ( 40.078 + 15.999 );
     fracM_O += (1-fracM)*fraction;
     rock->AddElement( elCa, fracM * fraction );
 
     // ============ Na2O =============
-    fraction = 0.0287;
-    fracM = 2*22.990 / ( 2*22.990 +15.999 );
+    fraction = 0.0642;
+    fracM = 2*22.990 / ( 2*22.990 + 15.999 );
     fracM_O += (1-fracM)*fraction;
     rock->AddElement( elNa, fracM * fraction );
 
     // ============ K2O =============
-    fraction = 0.0021;
-    fracM = 2*39.098 / ( 2*39.098 +15.999 );
+    fraction = 0.0347;
+    fracM = 2*39.098 / ( 2*39.098 + 15.999 );
     fracM_O += (1-fracM)*fraction;
     rock->AddElement( elK, fracM * fraction );
 
     // ============ P2O5 =============
-    fraction = 0.0007;
-    fracM = 2*30.974 / ( 2*30.974 +5*15.999 );
+    fraction = 0.0018;
+    fracM = 2*30.974 / ( 2*30.974 + 5*15.999 );
     fracM_O += (1-fracM)*fraction;
     rock->AddElement( elP, fracM * fraction );
 
     // ============ H2O =============
-    fraction = 0.107;
-    fracM = 2*1.008 / ( 2*1.008 +15.999 );
+    fraction = 0.0097;
+    fracM = 2*1.008 / ( 2*1.008 + 15.999 );
     fracM_O += (1-fracM)*fraction;
     rock->AddElement( elH, fracM * fraction );
 
+    // ============ CO2 =============
+	fraction = 0.0096;
+    fracM = 12.011 / ( 12.011 + 2*15.999 );
+    fracM_O += (1-fracM)*fraction;
+    rock->AddElement( elH, fracM * fraction );
+    
+	// ============ S ==============
+	fraction = 0.0001;
+	rock->AddElement( elS, fraction );
+
     // ============ Oxygen =============
     rock->AddElement( elO, fracM_O );
-    materialColor["rock"] = G4Color(G4Colour::Brown());
+    
+	materialColor["rock"] = G4Color(G4Colour::Brown());
 
     G4cout << *( G4Material::GetMaterialTable() ) << G4endl;
 }
